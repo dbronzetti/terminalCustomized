8c2ab5eb810d12cd7861923b8ecacd19ebfd94c6
dbronzetti <damianbronzetti@gmail.com>
2024-02-01 10:58:24 +0100

    Update alias.zsh

--- .config/alias.zsh
+++ .config/alias.zsh
@@ -1,14 +1,31 @@
+#Console
 alias cls='clear'
 alias ll='cls;ls -ltrG'
 alias la='cls;ll -a'
 alias reload='. ~/.zshrc'
+alias volumes='cd /Volumes/'
+
+#Git
 alias gpforce='git push --force $(git remote -v|grep origin|awk '\''FNR <= 1 { print $1 }'\'') $(git branch | sed -n -e "s/^\* \(.*\)/\1/p")'
 alias gpush='git push $(git remote -v|grep origin|awk '\''FNR <= 1 { print $1 }'\'') $(git branch | sed -n -e "s/^\* \(.*\)/\1/p")'
 alias gstatus='git status'
 alias gadd='git add -p'
 alias gcempty='git commit --amend --allow-empty'
 alias grebase="git fetch origin master && git rebase origin/master"
-alias reload="source ~/.zshrc"
+alias sshtest="ssh -T git@github.com"
+
+#Robly
+alias vr='cd ~/Documents/projects/robly/ && vagrant up && vagrant ssh'
+alias vl='cd ~/Documents/projects/lead/ && vagrant up && vagrant ssh'
+alias vrssh='cd ~/Documents/projects/robly/ && vagrant ssh'
+
+#TalentHack
+alias rebuild-test='~/Documents/projects/talent_hack/rebuild_test.sh'
+alias rc-th='heroku run --app release-thetalenthack rails c'
+alias rc-staging='heroku run --app staging-thetalenthack rails c'
+alias rc-preprod='heroku run --app staging-th rails c'
+
+#Job&Talent
 alias aws-prod="aws --profile production ecr get-login-password | docker login --username AWS --password-stdin 471410223680.dkr.ecr.eu-west-1.amazonaws.com"
 alias aws-stg="aws --profile staging ecr get-login-password | docker login --username AWS --password-stdin 471410223680.dkr.ecr.eu-west-1.amazonaws.com"
 alias gw-tunnel="ssh -NL 8085:es-service-gateway.stg.jobandtalent.cloud:80 deploy@gateway.staging.jobandtalent.com"
@@ -27,4 +44,16 @@ alias teleport-login-stg="tsh kube login jt-stg-products-eks-00"
 alias teleport-login-prd="tsh kube login jt-prd-products-eks-00"
 alias linter="bundle exec jt-linter --parallel"
 alias updevkit="devkit up -d auth auth-front backoffice candidates candidates-consumers candidates-workers companies companies-consumers companies-workers enrolment-es enrolment-es-chrome-browser enrolment-es-workers es-service-gateway farming farming farming-consumers farming-workers gateway integrations-consumers landings minichef staffing staffing-consumers staffing-workers transactions transactions-workers workforce workforce-consumers workforce-workers zeppelin"
-alias sshtest="ssh -T git@github.com"
+
+#Docker
+alias dkstop='docker stop $(docker ps -q)'
+alias dkbash='docker-compose run web bash'
+alias dkrails='docker run -ti -v "$PWD":/app -p 3000:3000 rails-base bash'
+alias gateway='route -n get default'
+
+#Rails
+alias migrate='rails db:migrate'
+alias console='rails c'
+alias routes='rails routes'
+alias test-console='rails c -e test'
+alias swagger='bundle exec rails rswag:specs:swaggerize'
