b2bfd7ae7c0bf84c1763c49c407d0fb65490656a
dbronzetti <damianbronzetti@gmail.com>
2024-02-01 10:58:59 +0100

    Delete .config/env.sh

--- .config/env.sh
+++
@@ -1,8 +0,0 @@
-export EDITOR=mvim
-export OS_TYPE=`uname`
-export ftp_host="/media/sf_ftp_host"
-
-export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
-
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
-
